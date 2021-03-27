ad648807182c7b39d099d28956aa3e362a718710
Chris Heimlich <chris@pcserenity.com>
2021-03-27 00:15:13 -0400

    Add project files.

---
+++ src/main.c
@@ -0,0 +1,26 @@
+#include <windows.h> 
+
+int main()
+{
+    // Unused crap
+    WCHAR* commandLine = L"";
+    STARTUPINFO dummySInfo;
+    PROCESS_INFORMATION dummyPInfo;
+    
+    // Prepare unused crap
+    ZeroMemory(&dummySInfo, sizeof(dummySInfo));
+    dummySInfo.cb = sizeof(dummySInfo);
+
+    // Change working dir
+    SetCurrentDirectory(L"Launcher");
+
+    // Do the thing
+    CreateProcess(L"Flashpoint.exe", commandLine, NULL, NULL, 0, NORMAL_PRIORITY_CLASS, NULL, NULL, &dummySInfo, &dummyPInfo);
+
+    // Cleanup more crap
+    CloseHandle(dummyPInfo.hProcess);
+    CloseHandle(dummyPInfo.hThread);
+
+    // World hunger is solved
+    return 0;
+}
---
+++ src/version.h
@@ -0,0 +1,21 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+#define VER_FILEVERSION             0,0,1,0
+#define VER_FILEVERSION_STR         "0.0.1.0"
+
+#define VER_PRODUCTVERSION          0,0,1,0
+#define VER_PRODUCTVERSION_STR      "0.0.1.0"
+
+#define VER_COMPANYNAME_STR         "oblivioncth"
+#define VER_FILEDESCRIPTION_STR     "Persistant starter for BlueMaxima's Flashpoint Launcher"
+#define VER_INTERNALNAME_STR        "StartFP"
+#define VER_LEGALCOPYRIGHT_STR      "Open Source @ 2021 oblivioncth"
+#define VER_LEGALTRADEMARKS1_STR    "All Rights Reserved"
+#define VER_LEGALTRADEMARKS2_STR    "GNU AGPL V3";
+#define VER_ORIGINALFILENAME_STR    "StartFP.exe"
+#define VER_PRODUCTNAME_STR         "FP Launcher Starter"
+
+#define VER_COMPANYDOMAIN_STR       "https://github.com/oblivioncth"
+
+#endif // VERSION_H
