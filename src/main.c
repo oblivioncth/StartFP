4e8b3e7f23d09a64ff409477f14bdf2b9a318cc2
Christian Heimlich <chris@pcserenity.com>
2022-08-06 18:56:58 -0400

    Clarify Win API usage, return 1 on process start failure

--- src/main.c
+++ src/main.c
@@ -3,22 +3,24 @@
 int main()
 {
     // Unused crap
-    STARTUPINFOW dummySInfo;
-    PROCESS_INFORMATION dummyPInfo;
+    STARTUPINFOW startupInfo;
+    PROCESS_INFORMATION processInfo;
     
-    // Prepare unused crap
-    ZeroMemory(&dummySInfo, sizeof(dummySInfo));
-    dummySInfo.cb = sizeof(dummySInfo);
+    // Initialize unused crap to defaults
+    ZeroMemory(&startupInfo, sizeof(startupInfo)); // Would be garbage otherwise
+    startupInfo.cb = sizeof(startupInfo); // Needs to know its own size because it could contain extended info
+    ZeroMemory(&processInfo, sizeof(processInfo)); // Would be garbage otherwise
 
     // Change working dir
-    SetCurrentDirectoryW(L"Launcher");
+    SetCurrentDirectoryW(L"Launcher"); // Could instead use 'lpCurrentDirectory' arg of CreateProcess, but CD'ing is how the shortcut does it
 
     // Do the thing
-    CreateProcessW(L"Flashpoint.exe", NULL, NULL, NULL, 0, NORMAL_PRIORITY_CLASS, NULL, NULL, &dummySInfo, &dummyPInfo);
+    if(!CreateProcessW(L"Flashpoint.exe", NULL, NULL, NULL, 0, NORMAL_PRIORITY_CLASS, NULL, NULL, &startupInfo, &processInfo))
+        return 1; // Mission failed, we'll get 'em next time
 
-    // Cleanup more crap
-    CloseHandle(dummyPInfo.hProcess);
-    CloseHandle(dummyPInfo.hThread);
+    // Cleanup unused crap handles (required)
+    CloseHandle(processInfo.hProcess);
+    CloseHandle(processInfo.hThread);
 
     // World hunger is solved
     return 0;
